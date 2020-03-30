b8176000f32ca4a1df404412fca817b59e915d96
cry <dutao183@163.com>
2020-03-30 14:19:48 +0800

    cryHouse

---
+++ cryHouse/setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages, setup
+
+
+"""
+    packages 告诉 Python 包所包括的文件夹（及其所包含的 Python 文件）。
+    find_packages() 自动找到这些文件夹，这样就不用手动写出来。
+    为了包含其他文件夹，如静态文件和模板文件所在的文件夹，需要设置 include_package_data 。
+    Python 还需要一个名为 MANIFEST.in 文件来说明这些文件有哪些。
+"""
+setup(
+    name='cryHouse',
+    version='1.0.0',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'flask',
+    ]
+)
